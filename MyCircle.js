42ac65fb3a6bdbc0c3e6c7b77a2b88f33cf40c87
ilona26gva <up201400035@fe.up.pt>
2017-10-21 18:09:58 +0100

    Update MyCircle.js
    added radius, texCoords

--- MyCircle.js
+++ MyCircle.js
@@ -2,9 +2,10 @@
  * MyCircle
  * @constructor
  */
- function MyCircle(scene, slices) {
+ function MyCircle(scene, r, slices) {
  	CGFobject.call(this,scene);
 	
+	this.r = r;
 	this.slices = slices;
 
  	this.initBuffers();
@@ -17,16 +18,19 @@
  	this.vertices = [];
  	this.normals = [];
  	this.indices = [];
+	this.texCoords = [];
 
 	var ang=(2*Math.PI)/this.slices;
 
 	for(j = 0; j < this.slices; j++) {
-		this.vertices.push(Math.cos(ang*j),Math.sin(ang*j),0);
+		this.vertices.push(this.r*Math.cos(ang*j),this.r*Math.sin(ang*j),0);
 		this.normals.push(0, 0, 1);
+		this.texCoords.push(0.5 + 0.5 * Math.cos(ang*j), 0.5 - 0.5 * Math.sin(ang*j));
 	}
 
 	this.vertices.push(0, 0, 0);
 	this.normals.push(0, 0, 1);
+	this.texCoords.push(0.5, 0.5);
 		
 	for(i = 0; i < this.slices - 1; i++) {
 		this.indices.push(i, i + 1, this.slices);
