5704224231b3223f587c6ec3666cabb2c024c71b
ilona26gva <up201400035@fe.up.pt>
2017-11-23 11:12:39 +0000

    Update MyGraphNode.js

--- MyGraphNode.js
+++ MyGraphNode.js
@@ -21,6 +21,9 @@ function MyGraphNode(graph, nodeID) {
     
     // The texture ID.
     this.textureID = null ;
+    
+    // The animation ID
+	this.animationID = null;
 
     this.transformMatrix = mat4.create();
     mat4.identity(this.transformMatrix);
